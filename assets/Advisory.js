ad19706df6f51d32324b6a6cc3cdb3028766bfef
Tamkeen Fatima <k.leotff786@gmail.com>
2021-08-18 21:06:33 +0530

    Add dummy data for testing

---
+++ assets/Advisory.js
@@ -0,0 +1,37 @@
+export default Advisory = [
+    {
+        id: 0,
+        name: 'Weather advisory',
+        advice: 'Advise for Weather'
+    },
+    {
+        id: 1,
+        name: 'Rice',
+        advice: 'Advise for Rice'
+    },
+    {
+        id: 2,
+        name: 'Fodder',
+        advice: 'Advise for Fodder'
+    },
+    {
+        id: 3,
+        name: 'Sugarcane',
+        advice: 'Advise for Sugarcane'
+    },
+    {
+        id: 4,
+        name: 'Vegetables',
+        advice: 'Advise for Vegetables'
+    },
+    {
+        id: 5,
+        name: 'Cattle',
+        advice: 'Advise for Cattle'
+    },
+    {
+        id: 6,
+        name: 'General Advisory',
+        advice: 'General Advisory'
+    }
+]
---
+++ assets/UserInfo.js
@@ -0,0 +1,10 @@
+export default UserInfo = {
+    block: 'Civil Lines',
+    district: 'Aligarh',
+    state: 'Uttar Pradesh',
+    date: 'Tuesday, 17 August 2021',
+    name: 'Test',
+    mobile: '6567389646',
+    IMEI: 'FGJ547r5',
+    language: 'English',
+}
