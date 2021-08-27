a21ae29f91cb169b569f95754c77b4dcce997337
Tamkeen Fatima <k.leotff786@gmail.com>
2021-08-27 19:08:17 +0530

    Add RootStackScreen

---
+++ src/components/RootStackScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LangSelectScreen from '../Screens/LangSelectScreen';
+import Welcome from '../Screens/WelcomeScreen';
+import LoginScreen1 from '../Screens/RegisterScreen';
+import LoginScreen from '../Screens/LoginScreen';
+
+const RootStack = createStackNavigator();
+
+export default function RootStackScreen() {
+    return (
+        <RootStack.Navigator screenOptions={{headerShown: false}}>
+            <RootStack.Screen name='LanguageSelect' component={LangSelectScreen} />
+            <RootStack.Screen name='Welcome' component={Welcome} />
+            <RootStack.Screen name='Register' component={LoginScreen1} />
+            <RootStack.Screen name='LogIn' component={LoginScreen} />
+        </RootStack.Navigator>
+    )
+}
